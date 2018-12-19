0a8aea90b4ea8809d00cfac06e0e66040845f380
Amarius1 <amarius2000@gmail.com>
2018-12-19 21:16:02 +0200

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,7 @@
+$scope.resetDashboard = function() {
+    //make field valid so that that gets filled into the ng-model & we can clear it.
+    $scope.DashboardForm.title.$setValidity('minlength', false)
+    $scope.DashboardForm.description.$setValidity('minlength', false)
+    $scope.dashboard.title = '';
+    $scope.dashboard.description = '';
+}
